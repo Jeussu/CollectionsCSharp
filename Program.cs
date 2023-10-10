43e05ae556e36f0270c9fd76596239790e66b26f
Jeussu <94780400+Jeussu@users.noreply.github.com>
2023-10-10 22:47:28 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿namespace CollectionsC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare and initialize array grades
+            int[] grades = new int[5];
+
+            grades[0] = 20;
+            grades[1] = 15;
+            grades[2] = 12;
+            grades[3] = 9;
+            grades[4] = 7;
+
+            Console.WriteLine("grades at index 0 : {0}", grades[0]);
+
+            string input = Console.ReadLine();
+            // assign value to arrray grades at index 0
+            grades[0] = int.Parse(input);
+            Console.WriteLine("grades at index 0 : {0}", grades[0]);
+
+
+            // onother way of initializing an array
+            int[] gradesOfMathStudentsA = { 20, 13, 12, 8, 7 };
+
+            // thrid way of initializing an array
+            int[] gradesOfMathStudentsB = new int[] { 15, 20, 3, 17, 18, 15 };
+
+            Console.WriteLine("Length of gradesOfMathStudentsA: {0}", gradesOfMathStudentsA.Length);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
